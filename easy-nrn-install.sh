eda0630aac0cb9a255c2c69cb80314c945754a7a
Tyler Banks <34382569+tjbanks@users.noreply.github.com>
2019-09-06 21:30:20 -0500

    Upgrade 7.5 to 7.7

--- easy-nrn-install.sh
+++ easy-nrn-install.sh
@@ -33,13 +33,13 @@ export PATH="$MPI_DIR/install/bin:$PATH"
 
 mkdir $NRN_DIR
 cd $NRN_DIR
-wget https://neuron.yale.edu/ftp/neuron/versions/v7.5/nrn-7.5.tar.gz
-wget https://neuron.yale.edu/ftp/neuron/versions/v7.5/iv-19.tar.gz
+wget https://neuron.yale.edu/ftp/neuron/versions/v7.7/nrn-7.7.tar.gz
+wget https://neuron.yale.edu/ftp/neuron/versions/v7.7/iv-19.tar.gz
 tar xzf iv-19.tar.gz
-tar xzf nrn-7.5.tar.gz
+tar xzf nrn-7.7.tar.gz
 # renaming the new directories iv and nrn makes life simpler later on
 mv iv-19 iv
-mv nrn-7.5 nrn
+mv nrn-7.7 nrn
 
 #Install IV
 
